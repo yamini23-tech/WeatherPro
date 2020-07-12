f221993cb7fc343f92ccf4fd4e3d5a590cc213f9
Akhil Reddy <akhilreddymedapati@gmail.com>
2020-07-12 15:48:13 -0400

    Content commited

---
+++ app/src/main/java/com/mobileapp/weatherpro/Constants.java
@@ -0,0 +1,28 @@
+package com.mobileapp.weatherpro;
+
+public class Constants {
+
+    public class City {
+
+        public static final String LONDON = "44418/";
+        public static final String INDIA = "2427032/";
+        public static final String SAN_FRAN = "2487956/";
+        public static final String DUBAI = "1940345/";
+        public static final String MOSCOW = "2122265/";
+        public static final String MONTREAL = "3534/";
+    }
+
+    public class WeatherStateAbbr {
+        public static final String CLEAR = "c";
+        public static final String HAIL = "h";
+        public static final String HEAVY_CLOUD = "hc";
+        public static final String HEAVY_RAIN = "hr";
+        public static final String LIGHT_CLOUD = "lc";
+        public static final String LIGHT_RAIN = "lr";
+        public static final String SHOWERS = "s";
+        public static final String SLEET = "sl";
+        public static final String SNOW = "sn";
+        public static final String THUNDERSTORM = "t";
+
+    }
+}
